6042c5032d93103ce852f3263d8e731e607ccfc2
= <=>
2020-04-06 00:38:17 -0500

    Refactored

--- main.r
+++ main.r
@@ -1,1 +1,20 @@
-## Waiting upload
+## COVID-19 Data Analysis with R
+##  Written by
+
+# assign data
+coronaData <- read.csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv")
+
+# head
+head(coronaData)
+
+# tail
+tail(coronaData)
+
+# returns a vector 
+coronaData$deaths
+
+# returns a dataframe
+coronaData["deaths"]
+
+
+
